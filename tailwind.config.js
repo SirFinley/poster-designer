19b02af2477a750c119e5d44c3881fabb5c605da
Dalton Wood <daltonwood.dev@gmail.com>
2021-11-01 15:22:24 -0500

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: "jit",
+    purge: [
+        './src/**/*.html',
+        './src/**/*.js',
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+};
