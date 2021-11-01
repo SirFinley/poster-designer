39e632e9ecb2ac1ca0d48820a13afb16b03b0ee2
Dalton Wood <daltonwood.dev@gmail.com>
2021-11-01 15:00:09 -0500

    adjust folder structure

--- src/js/poster.ts
+++ src/js/poster.ts
@@ -1,9 +1,13 @@
 import { fabric } from "fabric";
-import Overlay from './src/overlay';
-import Settings from './src/settings';
+import Overlay from './overlay';
+import Settings from './settings';
+import PosterImage from './image';
+
 const canvas = new fabric.Canvas('canvas');
 let settings = new Settings();
-let overlay = new Overlay(canvas);
 // TODO: read settings from document.referrer
 // readSettingsFromUrl(document.referrer);
 settings.readSettingsFromUrl(window.location.toString());
+
+let overlay = new Overlay(canvas, settings);
+let image = new PosterImage(canvas, settings);
