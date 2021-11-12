dfa08e9438ef5e254d8d13a3b17f8c9b416ebc6e
Dalton Wood <daltonwood.dev@gmail.com>
2021-11-12 13:49:23 -0600

    lambda to generate presigned url to upload to s3

--- lambda/bin/index.js
+++ lambda/bin/index.js
@@ -1,8 +1,44 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 const claudia_api_builder_1 = __importDefault(require("claudia-api-builder"));
+const aws_sdk_1 = require("aws-sdk");
+const crypto_1 = require("crypto");
 const api = new claudia_api_builder_1.default();
-api.get('/', () => 'Hello world');
+api.get('/', (event) => stuff(event));
+const URL_EXPIRATION_SECONDS = 5 * 60;
+function stuff(event) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const key = (0, crypto_1.randomUUID)();
+        const contentType = 'image/jpeg';
+        // Get signed URL from S3
+        let s3 = new aws_sdk_1.S3();
+        const s3Params = {
+            // Bucket: process.env.UploadBucket,
+            Bucket: 'visual-inkworks-dev',
+            Key: key,
+            Expires: URL_EXPIRATION_SECONDS,
+            ContentType: contentType,
+            // This ACL makes the uploaded object publicly readable. You must also uncomment
+            // the extra permission for the Lambda function in the SAM template.
+            // ACL: 'public-read'
+        };
+        const uploadUrl = yield s3.getSignedUrlPromise('putObject', s3Params);
+        return JSON.stringify({
+            event,
+            uploadUrl,
+            key,
+        });
+    });
+}
 module.exports = api;
--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -1,6 +1,37 @@
 import ApiBuilder from 'claudia-api-builder';
+import { S3 } from 'aws-sdk';
+import { randomUUID } from 'crypto';
 
 const api = new ApiBuilder();
-api.get('/', () => 'Hello world');
+api.get('/', (event) => stuff(event));
 
 export = api;
+
+const URL_EXPIRATION_SECONDS = 5 * 60;
+async function stuff(event) {
+
+	const key = randomUUID();
+	const contentType = 'image/jpeg';
+
+	// Get signed URL from S3
+	let s3 = new S3();
+	const s3Params = {
+		// Bucket: process.env.UploadBucket,
+		Bucket: 'visual-inkworks-dev',
+		Key: key,
+		Expires: URL_EXPIRATION_SECONDS,
+		ContentType: contentType,
+
+		// This ACL makes the uploaded object publicly readable. You must also uncomment
+		// the extra permission for the Lambda function in the SAM template.
+
+		// ACL: 'public-read'
+	}
+
+	const uploadUrl = await s3.getSignedUrlPromise('putObject', s3Params);
+	return JSON.stringify({
+		event,
+		uploadUrl,
+		key,
+	});
+}
