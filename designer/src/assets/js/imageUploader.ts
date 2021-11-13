3ca0c9d8d1589c94a4fe5f0f9bc2ed4990b44c01
Dalton Wood <daltonwood.dev@gmail.com>
2021-11-13 16:53:10 -0600

    use axios for upload, add progress bar

--- designer/src/assets/js/imageUploader.ts
+++ designer/src/assets/js/imageUploader.ts
@@ -1,68 +1,121 @@
 import PosterEventHub from "./posterEventHub";
+import axios from 'axios';
 
 export default class ImageUploader {
     constructor(eventHub: PosterEventHub) {
         this.eventHub = eventHub;
 
+        this.imageUploadCard = document.getElementById("img-upload-card") as HTMLElement;
+        this.imagePreviewElem = document.getElementById("img-preview") as HTMLImageElement;
+        this.imagePreviewSpinnerElem = document.getElementById("img-preview-spinner") as HTMLElement;
+        this.progressElem = document.getElementById("img-progress") as HTMLProgressElement;
+        this.progressLabelElem = document.getElementById("img-progress-label") as HTMLLabelElement;
         this.controller = new AbortController();
         this.signal = this.controller.signal;
+
+        this.eventHub.subscribe('imageChanged', () => {
+            showElement(this.imagePreviewElem, true);
+            showElement(this.imagePreviewSpinnerElem, false);
+        })
     }
 
     eventHub: PosterEventHub;
     controller: AbortController;
+    imageUploadCard: HTMLElement;
+    imagePreviewElem: HTMLImageElement;
+    imagePreviewSpinnerElem: HTMLElement;
+    progressElem: HTMLProgressElement;
+    progressLabelElem: HTMLLabelElement;
     signal: AbortSignal;
 
-    async start(file: File, progressCallback?: (progress: number) => void) {
-        const uploadUrl = await this.getPresignedUrl(file);
-        this.uploadFile(uploadUrl.uploadUrl, file);
+    async start(file: File) {
+        // this.initializeProgress();
+        // let interval = setInterval(() => {
+        //     this.updateProgress(this.progressElem.value + 10);
+        // }, 100);
+        // setTimeout(()=>{
+        //     clearInterval(interval);
+        // }, 1100);
+        // return;
+        try {
+            this.initializeProgress();
+            const uploadUrl = await this.getPresignedUrl(file);
+            this.uploadFile(uploadUrl.uploadUrl, file);
+        } catch (error) {
+            
+        }
     }
 
     async getPresignedUrl(file: File): Promise<GetUploadUrlResponse> {
-        const url = new URL("https://ot3uw7itn6.execute-api.us-east-1.amazonaws.com/latest");
-        url.search = new URLSearchParams({
-            contentType: file.type,
-        }).toString();
+        const url = "https://ot3uw7itn6.execute-api.us-east-1.amazonaws.com/latest";
 
-        let response = await fetch(url.toString(), {
-            method: 'GET',
+        let response = await axios.get<GetUploadUrlResponse>(url, {
             signal: this.signal,
+            params: {
+                contentType: file.type,
+            },
         })
-            .then((response) => response.json())
-            .catch((err) => {
-                if (err.name === 'AbortError') {
-                    console.error('Fetch aborted')
-                } else {
-                    console.error('Error while getting upload url: ', err)
-                }
+        .catch((err) => {
+            if (err.name === 'AbortError') {
+                console.error('Fetch aborted')
+            } else {
+                console.error('Error while getting upload url: ', err)
+                throw err;
+            }
 
-                this.eventHub.triggerEvent('imageUploadCancelled');
-            });
+            this.eventHub.triggerEvent('imageUploadCancelled');
+        });
 
-        return response as GetUploadUrlResponse;
+        if (!response) {
+            throw new Error("");
+        }
+
+        return response.data;
     }
 
     uploadFile(signedUrl: string, file: File) {
-        fetch(signedUrl, {
-            method: 'PUT',
-            body: file,
-            signal: this.signal,
-        })
-            .then((response) => {
-                return response.text();
-            })
-            .then((payload) => {
-                console.log(payload);
-            })
-            .catch((err) => {
-                if (err.name === 'AbortError') {
-                    console.error('Fetch aborted')
-                } else {
-                    console.error('Error while uploading: ', err)
-                }
+        // let data = new FormData();
+        // data.append('file', file);
 
-                this.eventHub.triggerEvent('imageUploadCancelled');
-            });
-        // TODO: indicate progress
+        axios.put(signedUrl, file, {
+            method: 'PUT',
+            signal: this.signal,
+            headers: {
+                'Content-Type': file.type,
+            },
+            onUploadProgress: (progressEvent) => {
+                let percent = Math.round(progressEvent.loaded / progressEvent.total * 100);
+                this.updateProgress(percent)
+            },
+        })
+        .catch((err) => {
+            if (err.name === 'AbortError') {
+                console.error('Fetch aborted')
+            } else {
+                console.error('Error while getting upload url: ', err)
+                throw err;
+            }
+
+            this.eventHub.triggerEvent('imageUploadCancelled');
+        });
+    }
+
+    initializeProgress(){
+        showElement(this.imageUploadCard, true);
+        showElement(this.imagePreviewElem, false);
+        showElement(this.imagePreviewSpinnerElem, true);
+        this.updateProgress(0);
+    }
+    
+    updateProgress(percent: number){
+        if (percent < 100){
+            this.progressLabelElem.innerText = `Uploading... ${percent}%`
+        }
+        else {
+            this.progressLabelElem.innerText = "Uploaded!";
+        }
+
+        this.progressElem.value = percent;
     }
 
     cancel() {
@@ -70,6 +123,14 @@ export default class ImageUploader {
     }
 }
 
+function showElement(elem: HTMLElement, show: boolean) {
+    if (show) {
+        elem.classList.remove('hidden');
+    }
+    else{
+        elem.classList.add('hidden');
+    }
+}
 interface GetUploadUrlResponse {
     uploadUrl: string,
     key: string,
