66e11ef3a6ef81102b85e58b3f2e98fd36b11d0a
Dalton Wood <daltonwood.dev@gmail.com>
2021-11-18 01:38:43 -0600

    render canvas

--- designer/src/assets/js/render.ts
+++ designer/src/assets/js/render.ts
@@ -11,33 +11,65 @@ export default class Render {
     settings: Settings;
 
     render(){
+        fabric.Object.NUM_FRACTION_DIGITS = 8;
+
         let clone = new fabric.Canvas(null);
         clone.loadFromJSON(this.canvas.toJSON(), () =>{
         // this.canvas.clone((clone: fabric.Canvas) => {
             clone.overlayImage = undefined;
             let objects = clone.getObjects();
+
+            let image: fabric.Image = new fabric.Image();
             objects.forEach(o => {
                 if (o.type != 'image') {
                     clone.remove(o);
                 }
+                else {
+                    image = o as fabric.Image;
+                }
             })
+
+            if (!image) {
+                return;
+            }
 
             clone.renderAll();
             console.log(clone);
 
             let dims = this.settings.getVirtualDimensions();
-            let multiplier = 2; // TODO: get multiplier based on image size, dpi, etc.
+
+            const imageRawWidth = image.width!;
+            const imageCanvasWidth = image.getScaledWidth();
+            
+            let canvasPixelsPerInch = 1 / dims.inchesPerPixel;
+            let imageWidthInInches = imageCanvasWidth / canvasPixelsPerInch;
+            let imagePixelsPerInch = imageRawWidth / imageWidthInInches;
+
+            let multiplier = 1 * imagePixelsPerInch / canvasPixelsPerInch; 
+
+            console.log('canvas dpi: ' + canvasPixelsPerInch);
+            console.log('print dpi: ' + imagePixelsPerInch);
+            console.log('render multiplier: ' + multiplier);
+
+            image.set({
+                left: Math.round(image.left!),
+                top: Math.round(image.top!),
+            })
 
             let dataUrl = clone.toDataURL({
-                width: dims.posterWidth,
-                height: dims.posterHeight,
-                left: dims.posterLeft,
-                top: dims.posterTop,
+                width: Math.ceil(dims.posterWidth),
+                height: Math.ceil(dims.posterHeight),
+                left: Math.round(dims.posterLeft),
+                top: Math.round(dims.posterTop),
+                // width: dims.posterWidth,
+                // height: dims.posterHeight,
+                // left: dims.posterLeft,
+                // top: dims.posterTop,
                 format: 'png',
                 multiplier,
             });
 
-            // document.getElementById('img-preview').src = dataUrl;
+            (document.getElementById('img-preview') as HTMLImageElement).src = dataUrl;
         })
     }
 }
