517d5f71b4654194bea43ae9253eb749fef229d7
Dalton Wood <daltonwood.dev@gmail.com>
2022-02-05 18:41:10 -0600

    add margin between canvas and settings

--- designer/src/App.tsx
+++ designer/src/App.tsx
@@ -1,20 +1,22 @@
-import Canvas from './components/Canvas';
-import IntroModal from './components/IntroModal';
-import Settings from './components/Settings';
-import { poster, PosterContext, SettingsContext } from './util/Context';
-import './App.css';
+import Canvas from "./components/Canvas";
+import IntroModal from "./components/IntroModal";
+import Settings from "./components/Settings";
+import { poster, PosterContext, SettingsContext } from "./util/Context";
+import "./App.css";
 
 function App() {
   return (
-    <div className="app p-2 gap-2">
-      <PosterContext.Provider value={poster}>
-        <SettingsContext.Provider value={poster.settings}>
-          <IntroModal></IntroModal>
-          <Canvas></Canvas>
-          <Settings></Settings>
-        </SettingsContext.Provider>
-      </PosterContext.Provider>
-    </div>
+    <>
+      <IntroModal></IntroModal>
+      <div className="app p-2">
+        <PosterContext.Provider value={poster}>
+          <SettingsContext.Provider value={poster.settings}>
+            <Canvas></Canvas>
+            <Settings></Settings>
+          </SettingsContext.Provider>
+        </PosterContext.Provider>
+      </div>
+    </>
   );
 }
 
